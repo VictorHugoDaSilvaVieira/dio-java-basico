dd3420545decab3f9f17f66cebba94a57aa837b0
Victor Hugo Da Silva Vieira <neveralgoz@uni9.edu.br>
2024-05-06 14:49:27 -0300

    implementacoes novas

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá Turma");
+        System.out.println("Boas Vindas turma Dio");
     }
 }
